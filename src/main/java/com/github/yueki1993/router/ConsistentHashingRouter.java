4f17884e3a54c44042aaa283a12557d2902e785e
Yohei Ueki <yohei_ueki@shino.ecei.tohoku.ac.jp>
2019-12-03 01:47:51 +0900

    impl virtual node

--- src/main/java/com/github/yueki1993/router/ConsistentHashingRouter.java
+++ src/main/java/com/github/yueki1993/router/ConsistentHashingRouter.java
@@ -6,6 +6,8 @@ import com.github.yueki1993.router.ring.Ring;
 import com.github.yueki1993.router.ring.TreeMapRing;
 
 import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class ConsistentHashingRouter implements Router.DynamicRouter {
@@ -13,7 +15,20 @@ public class ConsistentHashingRouter implements Router.DynamicRouter {
     private Hash hashFunction = new MD5Hash();
     private Ring ring = new TreeMapRing();
 
-    public ConsistentHashingRouter(Set<String> initialNodes) {
+
+    private Map<String, String> virtualToRealNode = new HashMap<>();
+    private Map<String, Set<String>> RealToVirtualNodes = new HashMap<>();
+    private int virtualNodeFactor;
+
+
+    public ConsistentHashingRouter(@Nonnull Set<String> initialNodes) {
+        this(initialNodes, 100);
+    }
+
+    public ConsistentHashingRouter(Set<String> initialNodes, int virtualNodeFactor) {
+        assert virtualNodeFactor >= 1;
+        this.virtualNodeFactor = virtualNodeFactor;
+
         for (String initialNode : initialNodes) {
             addNode(initialNode);
         }
@@ -22,14 +37,27 @@ public class ConsistentHashingRouter implements Router.DynamicRouter {
     @Nonnull
     public String getRoute(@Nonnull String target) {
         long hashedValue = hashFunction.getHash(target);
-        return ring.getSuccessor(hashedValue);
+        return ring.getSuccessor(hashedValue).realNode;
     }
 
     public void addNode(@Nonnull String node) {
-        ring.putNode(node, hashFunction.getHash(node));
+        for (int i = 0; i < virtualNodeFactor; i++) {
+            String virtualNodeName = getVirtualNodeName(node, i);
+            ring.putNode(new VirtualNode(virtualNodeName, node), hashFunction.getHash(virtualNodeName));
+        }
+
     }
 
     public void removeNode(@Nonnull String node) {
-        ring.removeNode(node, hashFunction.getHash(node));
+        for (int i = 0; i < virtualNodeFactor; i++) {
+            String virtualNodeName = getVirtualNodeName(node, i);
+            ring.removeNode(new VirtualNode(virtualNodeName, node), // not cool...
+                    hashFunction.getHash(node));
+        }
     }
+
+    private static String getVirtualNodeName(String realNodeName, int i) {
+        return realNodeName + "_" + i;
+    }
+
 }
